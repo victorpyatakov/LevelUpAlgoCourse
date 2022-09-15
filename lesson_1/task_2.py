3deff1e9607fe6b73b2fc101336c78c9701ad1a2
piatakov.v <piatakov.v@selectel.ru>
2022-09-15 18:02:53 +0300

    fix task 2

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -2,19 +2,17 @@
 
 def delete_number_in_arr(del_num: int, arr: list[int]) -> list[int]:
     if isinstance(del_num, int):
-        i = 0
-        while i < len(arr):
-            if arr[i] == del_num:
-                arr.pop(i)
-                continue
-            i += 1
-
-        return arr
+        j = 0
+        for i in range(len(arr)):
+            if arr[i] != del_num:
+                arr[j] = arr[i]
+                j += 1
+        return arr[:j]
     else:
         raise TypeError("del_num must be int")
     
 def test_correct():
-    assert delete_number_in_arr(4, [1, 2, 3, 4, 4, 5]) == [1,2,3,5]
+    assert delete_number_in_arr(4, [1, 2, 3, 4, 4, 5, 4]) == [1,2,3,5]
 
 def test_wrong_format():
     try:
@@ -28,8 +26,12 @@ def test_empty_arr():
 def test_del_num_not_in_arr():
     assert delete_number_in_arr(1, [2,2,2]) == [2,2,2]
 
+def test_all_el_deleted():
+        assert delete_number_in_arr(2, [2,2,2]) == []
+
 if __name__ == "__main__":
     test_correct()
+    test_all_el_deleted()
     test_wrong_format()
     test_empty_arr()
     test_del_num_not_in_arr()
