97abbd7d13bb95988403a90450eaa51e547d874c
piatakov.v <piatakov.v@selectel.ru>
2022-09-16 12:37:38 +0300

    fix task 5

--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -1,9 +1,24 @@
-"""Посчитать количество единиц в двоичной записи числа
+"""Посчитать количество единиц в двоичной записи числа"""
 
-5 = 101 = 2"""
+def get_count_ones(num: int) -> int:
+    if isinstance(num, int):
+        count_ones = 0
+        while num:
+            count_ones += num & 1
+            num = num >> 1
+        return count_ones
+    else:
+        raise TypeError("Input atribute must be int")
 
-def get_count_one(num: int) -> int:
-    ...
+def test_good():
+    assert get_count_ones(5) == 2
+
+def test_wrong_format():
+    try:
+        get_count_ones('9')
+    except TypeError as te:
+        assert str(te) == "Input atribute must be int"
 
 if __name__ == "__main__":
-    ...
+    test_good()
+    test_wrong_format()
