c96b7fd3d4754bcb9080f710957346b8e91fe6af
piatakov.v <piatakov.v@selectel.ru>
2022-09-19 22:45:07 +0300

    fix task 2 1

--- lesson_2/task_2_1.py
+++ lesson_2/task_2_1.py
@@ -7,27 +7,26 @@ def get_sorted_concat_arr(arr_1: list[int], arr_2: list[int]) -> list[int]:
     i = j = 0 # указатель на положение 1 и 2 массива
     result = []
     while i + j <  len(arr_1) + len(arr_2):
-        if i < len(arr_1) and j < len(arr_2):
-            if arr_1[i] >= arr_2[j]:
-                result.append(arr_2[j])
-                j += 1
-            else:
-                result.append(arr_1[i])
-                i += 1
-        else: # если длина разная
-            if i >= len(arr_1):
-                result.append(arr_2[j])
-                j += 1
-            else:
-                result.append(arr_1[i])
-                i += 1
+        if i >= len(arr_1):
+            result += arr_2[j:]
+            break
+        elif j >= len(arr_2):
+            result += arr_1[i:]
+            break
+        if arr_1[i] >= arr_2[j]:
+            result.append(arr_2[j])
+            j += 1
+        else:
+            result.append(arr_1[i])
+            i += 1
+
     return result
 
 def test_equal_size():
     assert get_sorted_concat_arr([1,3,5,7], [2,4,6,8]) == [1, 2, 3, 4, 5, 6, 7, 8]
 
 def test_diff_size():
-    assert get_sorted_concat_arr([1,3,5,7], [2,4,6]) == [1, 2, 3, 4, 5, 6, 7]
+    assert get_sorted_concat_arr([1,3,5,7], [2,4]) == [1, 2, 3, 4, 5, 7]
 
 def test_empty_list():
     assert get_sorted_concat_arr([],[]) == []
