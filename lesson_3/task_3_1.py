f9fa41099329833445cb57d0ed5eeec565849fd3
piatakov.v <piatakov.v@selectel.ru>
2022-09-22 01:04:43 +0300

    resolve choice sort

---
+++ lesson_3/task_3_1.py
@@ -0,0 +1,31 @@
+"""Реализовать сортировку выбором"""
+
+def choice_sort(arr: list[int]) -> list[int]:
+    if len(arr) == 0:
+        return []
+
+    point = 0
+
+    while point < len(arr) - 1:
+        min_index = point
+
+        for index in range(point + 1, len(arr)):
+            if arr[index] < arr[min_index]:
+                min_index = index
+        
+        tmp = arr[point]
+        arr[point] = arr[min_index]
+        arr[min_index] = tmp
+
+        point += 1
+
+    return arr
+
+def test_correct():
+    arr = [5, 3, 8, 16, 9, 1]
+    arr = choice_sort(arr)
+    assert arr == [1, 3, 5, 8, 9, 16]
+
+if __name__ == "__main__":
+    test_correct()
+        
