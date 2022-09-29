9c90c6df053fd4a5a472f94529aeaa8dac96f3e6
piatakov.v <piatakov.v@selectel.ru>
2022-09-29 17:57:03 +0300

    some fix

--- lesson_5/task_5_1.py
+++ lesson_5/task_5_1.py
@@ -1,34 +1,39 @@
 """Реализовать хещ таблицу"""
 
+
+
+
+def hash_func(arr, value):
+    siple_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 
+                            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 
+                            101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 
+                            151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+    sum = 0
+    for s in str(value):
+        sum += siple_numbers[ord(s) % len(siple_numbers)]
+    return sum % len(arr)
+
 class HashTable:
 
-    def __init__(self, size: int) -> None:
+    def __init__(self, size: int, hash_func: callable) -> None:
         self.values = [[] for _ in range(size)]
-        self.siple_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 
-                              43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 
-                              101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 
-                              151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+        self.__hash_func = hash_func
 
 
-    def __hash_func(self, value):
-        sum = 0
-        for s in str(value):
-            sum += self.siple_numbers[ord(s) % len(self.siple_numbers)]
-        return sum % len(self.values)
+    def add(self, value: list[any, any]): 
+        position = self.__hash_func(self.values, value[0])
+        self.values[position].append(value[1])
 
-    def add(self, value): 
-        self.values[self.__hash_func(value)].append(value)
-
-    def delete(self, value): 
-        position = self.__hash_func(value) 
+    def delete(self, value: list[any, any]): 
+        position = self.__hash_func(self.values, value[0]) 
         for i in range(len(self.values[position])):
-            if self.values[position][i] == value:
+            if self.values[position][i] == value[1]:
                 del self.values[position][i]
                 
-    def search(self, value): 
-        position = self.__hash_func(value) 
+    def search(self, value: list[any, any]): 
+        position = self.__hash_func(self.values, value[0]) 
         for i in self.values[position]:
-            if i == value:
+            if i == value[1]:
                 return True
         return False
 
@@ -38,20 +43,20 @@ class HashTable:
             
 
 def test_add():
-    hash_table = HashTable(4)
-    hash_table.add(15)
-    assert str(hash_table) == "['0: []', '1: []', '2: [15]', '3: []']"
+    hash_table = HashTable(4, hash_func=hash_func)
+    hash_table.add(['first', 15])
+    assert str(hash_table) == "['0: []', '1: []', '2: []', '3: [15]']"
 
 
 def test_search():
-    hash_table = HashTable(4)
-    hash_table.add(15)
-    assert hash_table.search(15) == True
+    hash_table = HashTable(4, hash_func=hash_func)
+    hash_table.add(['first', 15])
+    assert hash_table.search(['first', 15]) == True
     
 def test_delete():
-    hash_table = HashTable(4)
-    hash_table.add(15)
-    hash_table.delete(15)
+    hash_table = HashTable(4, hash_func=hash_func)
+    hash_table.add(['first', 15])
+    hash_table.delete(['first', 15])
     assert str(hash_table) == "['0: []', '1: []', '2: []', '3: []']"
 
 if __name__ == "__main__":
