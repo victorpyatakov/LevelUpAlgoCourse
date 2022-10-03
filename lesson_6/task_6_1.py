069dd08eddd9376289bb3dea08d0490307716528
piatakov.v <piatakov.v@selectel.ru>
2022-10-03 10:17:05 +0300

    find path

--- lesson_6/task_6_1.py
+++ lesson_6/task_6_1.py
@@ -3,33 +3,42 @@ from collections import deque
 
 def make_graph(): 
     graph = {
-        1: [2, 3, 4],
-        2: [5, 6],
-        3: [7, 9],
-        4: [10],
+        0: [1, 2, 3],
+        1: [4, 5],
+        2: [6, 8],
+        3: [9],
+        4: [],
         5: [],
-        6: [],
-        7: [8],
+        6: [7],
+        7: [],
         8: [],
-        9: [],
-        10: []
+        9: []
     }
     return graph
 
-def bfs(graph: dict, begin_vertex: int):
-    search_queue = deque()
-    search_queue += graph[begin_vertex]
-    searched = []
-    searched.append(begin_vertex)
+def bfs(graph: dict, start_vertex: int, end_vertex: int):
+    N = len(graph)
+    search_queue = deque([start_vertex])
+    distances = [None] * N
+    distances[start_vertex] = 0
+    parents = [None] * N
     while search_queue:
-        vertex = search_queue.popleft()
-        if vertex not in searched:
-            search_queue += graph[vertex]
-            searched.append(vertex)
-    return searched
+        u = search_queue.popleft()
+        for v in graph[u]:
+            if distances[v] is None:
+                distances[v] = distances[u] + 1
+                parents[v] = u
+                search_queue.append(v)
+    path = [end_vertex]
+    parent = parents[end_vertex]
+    while parent is not None:
+        path.append(parent)
+        parent = parents[parent]
+    
+    return path[::-1]
 
 def test_bfs():
-    assert bfs(make_graph(), 1) == [1,2,3,4,5,6,7,9,10,8]
+    assert bfs(make_graph(), 0, 9) == [0, 3, 9]
 
 if __name__ == "__main__":
     test_bfs()
