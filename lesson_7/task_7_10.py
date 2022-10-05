75dbea45f55bdd69004bcb41d858c621a52c2fd7
piatakov.v <piatakov.v@selectel.ru>
2022-10-05 20:11:00 +0300

    fix 7_10

--- lesson_7/task_7_10.py
+++ lesson_7/task_7_10.py
@@ -2,30 +2,33 @@
 
 def rev_sub_str_in_str(rev_str):
     def _swap(swap_str, i, j):
-        begin = i
-        end = j - 1
-        while begin < end:
-            tmp = swap_str[begin]
-            swap_str[begin] = swap_str[end]
-            swap_str[end] = tmp
-            begin += 1
-            end -= 1 
+        while i < j:
+            swap_str[i], swap_str[j] = swap_str[j], swap_str[i]
+            i , j =  i + 1, j - 1
 
-    i = 0
-    j = 0
+    def _get_next_char(rev_str, index) -> int:
+        while rev_str[index] == '_':
+            index += 1
+            if index == len(rev_str): break
+        return index
+
+    if not rev_str: return ""
+
+    i, j = 0, 0
 
     while i < len(rev_str):
+        i = _get_next_char(rev_str, i)
+        if i == len(rev_str): break
+        if i > j: j = i
         if j == len(rev_str):
-            _swap(rev_str, i , j)
+            _swap(rev_str, i , j - 1)
             break
         if rev_str[j] == '_' :
-            _swap(rev_str, i , j)
-            i = j + 1
-            j += 1
-        else:
-            j += 1
+            _swap(rev_str, i , j - 1)
+            j, i = j + 1, j + 1
+        else: j += 1
 
-    _swap(rev_str, 0, len(rev_str))
+    _swap(rev_str, 0, len(rev_str) - 1)
 
     return "".join(rev_str)
     
@@ -33,6 +36,26 @@ def test_reverse():
     test_str = "ab_cde_fghj"
     assert rev_sub_str_in_str(list(test_str)) == "fghj_cde_ab"
 
+def test_spaces():
+    test_str = "___ab____cde___fghj___"
+    assert rev_sub_str_in_str(list(test_str)) == "___fghj___cde____ab___"
+
+def test_all_space():
+    test_str = "_____________"
+    assert rev_sub_str_in_str(list(test_str)) == "_____________"
+
+def test_all_char():
+    test_str = "abgd"
+    assert rev_sub_str_in_str(list(test_str)) == "abgd"
+
+def test_epty():
+    test_str = ""
+    assert rev_sub_str_in_str(list(test_str)) == ""
+
 if __name__ == "__main__":
     test_reverse()
+    test_spaces()
+    test_all_space()
+    test_all_char()
+    test_epty()
     
